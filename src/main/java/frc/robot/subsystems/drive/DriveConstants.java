da8e3e26bb556d363124e88e09f143c5586aacef
Emmanuel <ejeffers22@gmail.com>
2024-12-31 17:15:30 -0500

    rev swerve simulation up and running

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -41,24 +41,24 @@ public class DriveConstants {
   public static final Rotation2d backRightZeroRotation = new Rotation2d(0.0);
 
   // Device CAN IDs
-  public static final int pigeonCanId = 9;
+  public static final int pigeonCanId = 2;
 
-  public static final int frontLeftDriveCanId = 1;
-  public static final int backLeftDriveCanId = 3;
-  public static final int frontRightDriveCanId = 5;
-  public static final int backRightDriveCanId = 7;
+  public static final int frontLeftDriveCanId = 10;
+  public static final int backLeftDriveCanId = 12;
+  public static final int frontRightDriveCanId = 11;
+  public static final int backRightDriveCanId = 13;
 
-  public static final int frontLeftTurnCanId = 2;
-  public static final int backLeftTurnCanId = 4;
-  public static final int frontRightTurnCanId = 6;
-  public static final int backRightTurnCanId = 8;
+  public static final int frontLeftTurnCanId = 20;
+  public static final int backLeftTurnCanId = 22;
+  public static final int frontRightTurnCanId = 21;
+  public static final int backRightTurnCanId = 23;
 
   // Drive motor configuration
-  public static final int driveMotorCurrentLimit = 50;
+  public static final int driveMotorCurrentLimit = 40;
   public static final double wheelRadiusMeters = Units.inchesToMeters(1.5);
   public static final double driveMotorReduction =
       (45.0 * 22.0) / (14.0 * 15.0); // MAXSwerve with 14 pinion teeth and 22 spur teeth
-  public static final DCMotor driveGearbox = DCMotor.getNeoVortex(1);
+  public static final DCMotor driveGearbox = DCMotor.getNEO(1);
 
   // Drive encoder configuration
   public static final double driveEncoderPositionFactor =
