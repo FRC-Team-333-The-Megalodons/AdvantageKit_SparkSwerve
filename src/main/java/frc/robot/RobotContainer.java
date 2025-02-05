166ffc384006988b0525ac17c3ac14e2e5fdcc3b
FRC-Team-333 <frcteam333themegalodons@gmail.com>
2025-02-04 21:50:21 -0500

    tuned swerve drive and broke elevator again
    Co-authored-by: Emmanuel <ejeffers22@gmail.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,7 +16,6 @@ package frc.robot;
 import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -165,11 +164,13 @@ public class RobotContainer {
     // Switch to X pattern when X button is pressed
     controller.square().onTrue(Commands.runOnce(drive::stopWithX, drive));
 
-    //TODO: Angelina add comments
+    // TODO: Angelina add comments
     controller.povUp().whileTrue(elevator.runPercent(0.5));
     controller.povDown().whileTrue(elevator.runPercent(-0.5));
+
     controller.triangle().whileTrue(intake.runPercent(0.75));
     controller.cross().whileTrue(intake.runPercent(-0.75));
+
     controller.R1().whileTrue(wrist.runPercent(0.2));
     controller.L1().whileTrue(wrist.runPercent(-0.2));
 
