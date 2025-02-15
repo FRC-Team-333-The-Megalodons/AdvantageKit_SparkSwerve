5972c6d5f9f4e316978f6aafa63b1ce7b1eb4c75
chereshenka0 <liss030780@gmail.com>
2025-02-15 11:21:35 -0500

    pulling set points for the elevator

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -14,21 +14,21 @@ public class ElevatorConstants {
   public static final double motorReduction = 1.0;
   public static final int currentLimit = 40;
 
-  public static final double ELEVATOR_MAX_HEIGHT = 0.0;
-  public static final double ELEVATOR_MIN_HEIGHT = 0.0;
+  public static final double ELEVATOR_MAX_HEIGHT = 345.9714;
+  public static final double ELEVATOR_MIN_HEIGHT = -5.88259;
 
-  public static final double ELEVATOR_HOME_POSITION = 0.0;
+  public static final double ELEVATOR_HOME_POSITION = -5.88259;
 
-  public static final double ELEVATOR_CORAL_PICKUP_POS = 0.0;
+  public static final double ELEVATOR_CORAL_PICKUP_POS = -5.88259;
   public static final double ELEVATOR_ALGAE_PICKUP_FLOOR_POS = 0.0;
 
-  public static final double ELEVATOR_ALGAE_SCORE_POS = 0.0;
+  public static final double ELEVATOR_ALGAE_SCORE_POS = 30.3395;
 
-  public static final double ELEVATOR_ALGAE_PICKUP_L2_POS = 0.0;
-  public static final double ELEVATOR_ALGAE_PICKUP_L3_POS = 0.0;
+  public static final double ELEVATOR_ALGAE_PICKUP_L2_POS = 119.8012;
+  public static final double ELEVATOR_ALGAE_PICKUP_L3_POS = 208.0858;
 
-  public static final double ELEVAOTR_SCORE_CORAL_L1_POS = 0.0;
-  public static final double ELEVATOR_SCORE_CORAL_L2_POS = 0.0;
-  public static final double ELEVATOR_SCORE_CORAL_L3_POS = 0.0;
-  public static final double ELEVATOR_SCORE_CORAL_L4_POS = 0.0;
+  public static final double ELEVAOTR_SCORE_CORAL_L1_POS = -5.88259;
+  public static final double ELEVATOR_SCORE_CORAL_L2_POS = 63.2797;
+  public static final double ELEVATOR_SCORE_CORAL_L3_POS = 163.7695;
+  public static final double ELEVATOR_SCORE_CORAL_L4_POS = 345.4402;
 }
