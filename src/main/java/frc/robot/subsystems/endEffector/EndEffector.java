e382e1bb01183be7fb7c6ab64d05a82e9bd98df1
amina <amina.akhmedova1704@gmail.com>
2025-02-27 15:56:37 -0500

    fixed the endeffector logger

--- src/main/java/frc/robot/subsystems/endEffector/EndEffector.java
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffector.java
@@ -7,7 +7,6 @@ package frc.robot.subsystems.endEffector;
 import com.ctre.phoenix6.hardware.CANrange;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.elevator.ElevatorIOInputsAutoLogged;
 
 // import frc.robot.subsystems.LEDStrip;
 import java.util.function.DoubleSupplier;
