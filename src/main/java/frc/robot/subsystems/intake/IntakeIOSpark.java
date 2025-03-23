8f37969c86401d23fc6f45e18a2cb0e1c4c94588
chereshenka0 <liss030780@gmail.com>
2025-03-23 14:05:23 -0400

    adding motion magic

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSpark.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSpark.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems.intake;
 import static frc.robot.subsystems.intake.IntakeConstants.*;
 import static frc.robot.util.SparkUtil.*;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase.ControlType;
@@ -25,6 +26,8 @@ public class IntakeIOSpark implements IntakeIO {
   private RelativeEncoder encoder = intake.getEncoder();
   SparkClosedLoopController m_controller = intake.getClosedLoopController();
   private CANcoder intakeEncoder = new CANcoder(6);
+  private TalonFXConfiguration newMagicMotion = new TalonFXConfiguration();
+
 
   // private PIDController pid = new PIDController(1.4, 0, 0);
   // private CANrange canRange = new CANrange(IntakeConstants.canRangeId);
@@ -41,6 +44,19 @@ public class IntakeIOSpark implements IntakeIO {
         .uvwMeasurementPeriod(10)
         .uvwAverageDepth(2);
     config.closedLoop.p(0.8).i(0).d(1.0);
+
+    var slot0Configs = newMagicMotion.Slot0;
+    slot0Configs.kS = 0.25; // Add 0.25 V output to overcome static friction
+    slot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+    slot0Configs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
+    slot0Configs.kP = 4.8; // A position error of 2.5 rotations results in 12 V output
+    slot0Configs.kI = 0; // no output for integrated error
+    slot0Configs.kD = 0.1; // A velocity error of 1 rps results in 0.1 V output
+
+    var motionMagicConfigs = newMagicMotion.MotionMagic;
+    motionMagicConfigs.MotionMagicCruiseVelocity = 80; // Target cruise velocity of 80 rps
+    motionMagicConfigs.MotionMagicAcceleration = 160; // Target acceleration of 160 rps/s (0.5 seconds)
+    motionMagicConfigs.MotionMagicJerk = 1600; // Target jerk of 1600 rps/s/s (0.1 seconds)
 
     tryUntilOk(
         intake,
